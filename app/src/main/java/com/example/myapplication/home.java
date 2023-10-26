f33a4640f1f60344c39cba49598f26f846d161e7
emomomo <emomomonotuse@gmail.com>
2023-10-26 23:45:37 +0800

    "second"

--- app/src/main/java/com/example/myapplication/home.java
+++ app/src/main/java/com/example/myapplication/home.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 
 public class home extends AppCompatActivity {
@@ -12,12 +11,14 @@ public class home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.home);
         // 获取底部导航栏
         BottomNavigationView bottomNavView = findViewById(R.id.bottomNavView);
-        BottomNavigationHelper.setupBottomNavigation(bottomNavView, this);
+
 
         // 设置底部导航栏的监听器
+        BottomNavigationHelper.setupBottomNavigation(bottomNavView, this);
 
     }
 }
